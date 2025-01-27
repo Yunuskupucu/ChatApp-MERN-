42a3945ab88dd4437d3b19dd9b6c2cdc3c996ffb
yunus kupucu <yunus.kupucu@gmail.com>
2025-01-27 18:53:02 +0300

    signup page completed

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -11,7 +11,7 @@ import {
 } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import AuthImagePattern from '../components/AuthImagePattern';
-
+import toast from 'react-hot-toast';
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
@@ -22,9 +22,25 @@ const SignUpPage = () => {
 
   const { signup, isSigningUp } = useAuthStore();
 
-  const validateForm = () => {};
+  const validateForm = () => {
+    if (!formData.fullName.trim()) return toast.error('Full Name is required');
+    if (!formData.email.trim()) return toast.error('Email is required');
+    if (!/\S+@\S+\.\S+/.test(formData.email))
+      return toast.error('Invalid email format');
+    if (!formData.password) return toast.error('Password is required');
+    if (formData.password.length < 6)
+      return toast.error('Password must be at least 6 characters');
+
+    return true;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const success = validateForm();
+
+    if (success) {
+      signup(formData);
+    }
   };
 
   return (
