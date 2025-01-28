d48542858f52259365b54954ed4418c3e91fb396
yunus kupucu <yunus.kupucu@gmail.com>
2025-01-29 01:12:32 +0300

    bug fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 
 import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
 const app = express();
@@ -21,6 +22,17 @@ app.use(
 );
 
 app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
+
+// app.put('/auth/update-profile', async (req, res) => {
+//   try {
+//     const updatedProfile = { success: true, data: { username: 'exampleuser' } };
+//     res.status(200).send(updatedProfile);
+//   } catch (error) {
+//     console.error('Error in /auth/update-profile:', error);
+//     res.status(500).send({ message: 'An error occurred on the server' });
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
